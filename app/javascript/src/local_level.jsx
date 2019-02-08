63108b87e4d9eaca7f5e40f223e2b7992bbc5b4f
Jevon Wright <jevon@fluxfederation.com>
2019-02-08 14:47:22 +1300

    Refactor Tile into a separate React component
    It's OK to have child React components within parent React components;
    I'm not sure why previously I was getting "Objects are not valid as a
    React child".
    
    Perhaps because I was trying to render an Object directly to the DOM,
    whereas now (since I'm now a bit more familiar with JSX syntax) I'm
    assigning the object directly as props.

--- app/javascript/src/local_level.jsx
+++ app/javascript/src/local_level.jsx
@@ -45,6 +45,46 @@ const TILE_CLASSES = {
   2: "floor",
 };
 
+class Tile extends React.Component {
+  render() {
+    // returns a string, not React
+    const renderMonsters = (monsters) => {
+      let monsterText = monsters.map((monster) => (
+        `${monster.type} (${monster.health} hp)`
+      ));
+
+      return monsterText.join(", ");
+    };
+
+    let tileText = [];
+
+    if (this.props.tile.monsters.length > 0) {
+      tileText.push(renderMonsters(this.props.tile.monsters));
+    }
+
+    if (this.props.tile.x == this.props.player.x && this.props.tile.y == this.props.player.y) {
+      tileText.push("(you're here!)");
+    }
+
+    let tileClasses = ["tile"];
+    let tileContent = TILE_CLASSES[parseInt(this.props.tile.tile, 10)];
+    tileClasses.push(tileContent || "unknown");
+
+    let button = "";
+    if (this.props.tile.visit_path) {
+      button = <a className="visit" href={this.props.tile.visit_path} data-method="post">visit</a>
+    }
+
+    return (
+      <td key={this.props.tile.x + "," + this.props.tile.y} className={tileClasses.join(" ")}>
+        <small className="debug">({this.props.tile.x}, {this.props.tile.y})</small>
+        {tileText.join(" ")}
+        {button}
+      </td>
+    );
+  }
+}
+
 class LocalLevel extends React.Component {
   render() {
     // valid, but ugh, so many loops
@@ -62,50 +102,17 @@ class LocalLevel extends React.Component {
     //   </tbody>
     // </table>
 
-    // returns a string, not React
-    const renderMonsters = (monsters) => {
-      let monsterText = monsters.map((monster) => (
-        `${monster.type} (${monster.health} hp)`
-      ));
-
-      return monsterText.join(", ");
-    };
-
-    const renderTile = (tile) => {
+    const renderRow = (row, index) => {
       // (...); is necessary for .jsx syntax.
       // {...; return X} is necessary to be a valid function
       //   AND to have if/lets/vars inside the block!
-      let tileText = [];
 
-      if (tile.monsters.length > 0) {
-        tileText.push(renderMonsters(tile.monsters));
-      }
-
-      if (tile.x == this.props.data.player.x && tile.y == this.props.data.player.y) {
-        tileText.push("(you're here!)");
-      }
-
-      let tileClasses = ["tile"];
-      let tileContent = TILE_CLASSES[parseInt(tile.tile, 10)];
-      tileClasses.push(tileContent || "unknown");
-
-      let button = "";
-      if (tile.visit_path) {
-        button = <a className="visit" href={tile.visit_path} data-method="post">visit</a>
-      }
-
-      return (
-        <td key={tile.x + "," + tile.y} className={tileClasses.join(" ")}>
-          <small className="debug">({tile.x}, {tile.y})</small>
-          {tileText.join(" ")}
-          {button}
-        </td>
+      const tableTiles = row.map((tile) =>
+        <Tile key={tile.x + "," + tile.y} tile={tile} player={this.props.data.player} />
       );
-    };
 
-    const renderRow = (row, index) => {
       return <tr key={"row" + index}>
-        { row.map(renderTile) }
+        { tableTiles }
       </tr>
     };
 
